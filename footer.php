5f837c6f170be10dfff9bbe889718cdd9e2f9492
Rinno399 <78363610+Rinno399@users.noreply.github.com>
2021-11-15 11:26:21 +0630

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,25 @@
+<style>
+#footer{
+
+}
+#lev{
+  color: cyan;
+  margin-top: 20px;
+  text-decoration: none;
+  border-radius: 13px;
+  padding: 15px;
+  margin-left: 7px;
+  border:0px;
+  box-shadow:inset 8px 8px 10px #333333,inset -8px -8px 10px #0d0d0d;
+}
+</style>
+<center>
+<div id="footer">
+<tr>
+<a href="lev-1.php" id="lev" target="_blank">lev-1</a>
+<a href="lev-2.php?cmd=ip" id="lev" target="_blank">lev-2</a>
+<a href="lev-3.php" id="lev" target="_blank">lev-3</a>
+</tr>
+</div>
+</center>
 
